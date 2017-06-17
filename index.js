7a3b44a4610dad005b0067e0ec70de2c456a163d
ChirajJ <chiraj.pep@gmail.com>
2017-06-17 18:15:14 +0530

    Added Procfile and modified port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
 app.get('/',function(req, res){res.sendFile(__dirname+"/index.html");});
-var port = 3000;
+var port = process.env.PORT || 8080;
 app.listen(port);
 
